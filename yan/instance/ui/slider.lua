d8bb07086f00eab21856e8d1f199f08caa73b971
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-06 11:49:25 -0600

    slider works now

--- yan/instance/ui/slider.lua
+++ yan/instance/ui/slider.lua
@@ -2,6 +2,8 @@ local slider = {}
 local guibase = require("yan.instance.ui.guibase")
 local Color = require("yan.datatypes.color")
 local UIVector2 = require("yan.datatypes.uivector2")
+local utils = require("yan.utils")
+
 slider.__index = guibase
 
 function slider:New(screen, minValue, maxValue)
@@ -9,16 +11,36 @@ function slider:New(screen, minValue, maxValue)
     o.Type = "Slider"
     o.Progress = 0.5
     o.Value = minValue
+    o.CornerRoundness = 8
     
     o.SliderColor = Color.new(0.7,0.7,0.7,0.5)
-    o.SliderSize = UIVector2.new(0,25,1,0)
+    o.SliderSize = UIVector2.new(0,100,1,0)
     
+    
+
+    o.WasSliding = false
+
     function o.Slide(mX, mY)
+        local pX, pY, sX, sY = o:GetDrawingCoordinates()
         
+        o.Progress = utils:Clamp((mX - pX) / sX, 0.0, 1.0)
+        o.Value = minValue + (maxValue - minValue) * o.Progress
+        print(o.Value)
     end
 
     function o:Draw()
+        local mX, mY = love.mouse.getPosition()
         local pX, pY, sX, sY = o:GetDrawingCoordinates()
+
+        if love.mouse.isDown(1) and (utils:CheckCollision(mX, mY, 1, 1, pX, pY, sX, sY) or o.WasSliding) then
+            o.WasSliding = true
+            o.Slide(mX, mY)
+        end
+
+        if not love.mouse.isDown(1) then
+            o.WasSliding = false
+        end
+        
         love.graphics.setColor(o.Color.R, o.Color.G, o.Color.B, o.Color.A)        
         love.graphics.rectangle("fill", pX, pY, sX, sY, o.CornerRoundness, o.CornerRoundness)
         
